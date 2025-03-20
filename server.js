3a9ef9bf5b9da178aa0258a833638793c255f855
Hemanth-konduri <konduri.hemanth.s89@gmail.com>
2025-03-20 12:07:03 +0530

    Completed

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+
+app.use(express.json());
+
+
+app.get('/ping', (req, res) => {
+    res.json({ message: 'pong' });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
