83342c5d6e64e994fb56397239ce907ed62aaae5
Hemanth-konduri <konduri.hemanth.s89@gmail.com>
2025-03-29 11:08:07 +0530

    ASAP_REACT APP Completed

---
+++ Frontend/react/src/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <h1>Project Title: 🤣🤣Accidental Goggle Searches Archive🤣🤣
+        </h1>
+        <h2>Project Overview: <p>This is a fun, user-generated archive where people can log in and submit the funniest or weirdest Google search mistakes they have made. Other users can upvote/downvote the most hilarious searches, comment on them, and even see a leaderboard of the top accidental searches.</p></h2>
+          <button>Know more 😊</button>
+
+    </div>
+  )
+}
+
+export default About
